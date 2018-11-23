b5c8ddf624dc69c02c7934b94ecdd54a562ace1a
British Systems <41692418+British-Systems@users.noreply.github.com>
2018-11-23 17:12:07 +0000

    Stub of Extension

---
+++ MultiPlug.Ext.QRCoder/Core.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MultiPlug.Ext.QRCoder
+{
+    public class Core
+    {
+        private static Core m_Instance = null;
+
+        public static Core Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = new Core();
+                }
+                return m_Instance;
+            }
+        }
+    }
+}
---
+++ MultiPlug.Ext.QRCoder/QRCoder.cs
@@ -0,0 +1,69 @@
+﻿using MultiPlug.Extension.Core;
+using System;
+using System.Collections.Generic;
+using MultiPlug.Base.Exchange;
+using MultiPlug.Extension.Core.Views;
+
+namespace MultiPlug.Ext.QRCoder
+{
+    public class QRCoder : ExtensionBase
+    {
+        public override MultiPlugType<ViewBase[]> Apps
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override List<Event> Events
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override List<Subscription> Subscriptions
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override event EventHandler<MultiPlugType<ViewBase[]>> AppsUpdated;
+        public override event EventHandler<List<Event>> EventsUpdated;
+        public override event EventHandler<List<Subscription>> SubscriptionsUpdated;
+
+        public override void Initialise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Load(KeyValuesJson[] config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnUnhandledException(UnhandledExceptionEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
